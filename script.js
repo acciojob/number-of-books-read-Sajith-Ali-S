04b5c58eeba572e72060d2df9bf8fbc0ced183b0
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-04-23 08:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-	let count =0;
-	for(let i in library){
-		if(library.readingStatus==true){
-			count++;
-		}
-	}
-	return count;
-};
+//   for(let i in library){
+//     console.log(library[i].readingStatus)
+//   }
+  
+  const numberOfBooksRead = () => {
+    // write your code here
+      let count =0;
+      for(let i in library){
+          if(library[i].readingStatus==true){
+              count++;
+          }
+      }
+      return count;
+      
+  };
 
+ // console.log(numberOfBooksRead());
+  
 // Do not change the code below
 
 alert(numberOfBooksRead());
